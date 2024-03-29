c0ecd7dccd059936ff5c00946451a40a9a9acbf7
Chen-Jin-yuan <1424277869@qq.com>
2024-03-29 15:01:29 +0800

    erms lab3

--- system/ERMS/erms_main.py
+++ system/ERMS/erms_main.py
@@ -18,7 +18,7 @@ nginxIP=os.popen("kubectl get pods -o wide | grep frontend | awk '{print $6}'").
 print(nginxIP)
 frontend_host = "http://"+nginxIP[0].replace("\n","")+":8080/"
 load_qps = 3000
-load_type = 7
+load_type = 8
     
 
 def load_func():
